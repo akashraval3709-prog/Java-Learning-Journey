79abf77ba690957728be83ef95bde1d573eca19d
akash <akashraval3709@gmail.com>
2026-07-14 22:05:00 +0530

    Add Greeter class to handle command-line argumentsfeat: add command-line arguments demonstration program
    Added a Java program demonstrating the use of command-line arguments. The program accepts a user's name as an argument, displays a personalized greeting when an argument is provided, and shows a default greeting when no arguments are passed.

---
+++ College-Practicals/11-Command-Line-Arguments/Greeter.java
@@ -0,0 +1,15 @@
+public class Greeter {
+
+    public static void main(String [] args){
+    
+
+       if(args.length == 1)
+       {
+        System.out.println("Hello, " +args[0]+"! Welcome to Java Class.");
+       }
+       else{
+        System.out.println("Hello, Guest!");
+       }
+    }
+    
+}
